8f28b5e19b96c2dd378dc98d5b796a210573802f
Suraj <suraj.kumar@arittek.com>
2022-12-09 15:09:13 +0500

    User Get Api

--- src/main/java/com/myfirstproject/myfirstproject/User.java
+++ src/main/java/com/myfirstproject/myfirstproject/User.java
@@ -1,5 +1,15 @@
 package com.myfirstproject.myfirstproject;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@RequestMapping(value = "/user")
 public class User {
+
+    @GetMapping
+    public String test() {
+        return "hello suraj";
+    }
 }
